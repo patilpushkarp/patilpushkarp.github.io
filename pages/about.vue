a007a56eb243231b84d04aa09b7ead9ec6509d9d
Pushkar Patil <patilpushkarp@gmail.com>
2024-08-21 00:25:42 +0530

    Updated about data

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Pushkar</h1>
+    <p class="name">{{ myData.name }}</p>
+    <div class="brief">{{ myData.brief }}</div>
 </template>
+
+<script>
+import aboutData from "./../data/about.json"
+
+export default{
+    data(){
+        return {
+            myData: aboutData
+        };
+    }
+}
+
+</script>
+
+<style scoped>
+.name{
+    padding: 5% 10%;
+    font-size: 5vw;
+    font-weight: 900;
+}
+
+.brief{
+    width: 50%;
+    font-size: 2vw;
+    font-weight: 300;
+    float: right;
+    padding-right: 5%;
+}
+</style>
